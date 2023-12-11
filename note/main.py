3687bc22c928af8788909a1f0f9fadd3b9c5add6
Martin Christoph Frank <martin7.frank7@gmail.com>
2023-12-11 22:03:57 +0100

    using simple magic and introducing teaching mode for now

--- note/main.py
+++ note/main.py
@@ -3,40 +3,56 @@ import subprocess
 import sys
 from pathlib import Path
 from pydantic import BaseModel
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
-from langchain.schema.output_parser import StrOutputParser
 
+import magic
 from model import FilePath
 
-def get_branch_prompt(name: str):
-    with open(f"./prompts/{name}.pt", "r") as f:
-        return f.read()
+MODE = "TEACH"
 
-def evolve(seed: str, prompt: str) -> str:
-    # runs an api call to some magic text to text engine
-    prompt_template = ChatPromptTemplate.from_template(prompt)
-    model = ChatOpenAI()
-    chain = prompt_template | model | StrOutputParser()
-    response = chain.invoke({"seed_text": seed})
-    return response
-    
-def branch(seed_text:str, name:str) -> str:
-    prompt = get_branch_prompt(name)
-    response = evolve(seed_text, prompt)
-    return response
+def grow(seed: str, target: str) -> str:
+    ## does magic and grows the seed into another representation
+    return magic.simple(seed, target)
+
+def create_new_note(branch_name):
+    """
+    in note mode:
+        just creates the note file and exits
+    in teaching mode:
+        lets the ai attempt to improve the note
+        lets the user improve the mutated string
+        improve or approve it
+    in auto mode:
+        uses the optimize.pt system message
+        this is a file you should definitely edit
+        goal is to improve it periodically
+
+    if success then returns true
+    else false
+    """
+    seed_file = FilePath.from_home_dir(branch_name)
+    seed_file.open_in_editor()
+
+    seed = seed_file.read_content()
+
+    if MODE == "NOTE":
+        return True
+    if MODE == "TEACH":
+        # perhaps randomly select from a list of aims
+        # buf for now just do the story one
+        # next will be the research one
+        text_target = grow(seed, "story")
+        improved_file= FilePath.from_home_dir('seeds')
+        improved_file.write_content(text_target + f"\n---\n[seed]({seed_file.path.name})\n---\n")
+        improved_file.open_in_editor()
+        return True
+    if MODE == "AUTO":
+        seed_new = grow(seed, "story")
+        text_target = grow(seed_new, "optimize")
+        return True
+    return False
 
 def main():
-    seed_file = FilePath.from_home_dir('seeds')
-    seed_file.open_in_editor()
-    
-    seed_text = seed_file.read_content()
-    tweet_text = branch(seed_text, "tweet")
-
-    tweet_file = FilePath.from_home_dir('tweets')
-    tweet_file.path = tweet_file.path.with_name(seed_file.path.name)  # Match the file name
-    tweet_file.write_content(tweet_text)
-    tweet_file.open_in_editor()
+    create_new_note("seeds")
 
 if __name__ == "__main__":
     main()
