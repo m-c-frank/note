8776c90eaede0a297a4dbfced4b4fc4944efa9d7
Martin Christoph Frank <martin7.frank7@gmail.com>
2024-01-14 16:02:43 +0100

    updating install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,7 @@ cd makecli
 bash ./install.sh
 git clone https://github.com/m-c-frank/note
 cd note
-result=$($HOME/tools/makecli -name="note" -source="main.go")
+result=$($HOME/mkcli -name="note" -source="main.go")
+rm -rf ./makecli
+rm -rf ./note
 echo "you can now delete the ./note dir"
