a857a654c32b4fcc08af4cc7fe848e969864246e
Martin Christoph Frank <martin7.frank7@gmail.com>
2024-01-18 07:28:32 +0100

    added server

---
+++ server/server.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/m-c-frank/note/api"
+)
+
+// Note represents the JSON structure for the note
+type Note struct {
+	Origin  string `json:"origin"`
+	Content string `json:"content"`
+}
+
+func main() {
+	// Create a Gin router
+	r := gin.Default()
+
+	// Define the /api/note endpoint
+	r.POST("/api/note", func(c *gin.Context) {
+		var rawNote Note
+
+		// Bind the JSON to the note struct
+		if err := c.BindJSON(&rawNote); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Print the note content
+		fmt.Printf("Received rawNote: %s\n", rawNote.Content)
+		fmt.Printf("From origin: %s\n", rawNote.Origin)
+
+		note := api.TakeNote(rawNote.Content, rawNote.Origin)
+		repoPath, err := api.DetermineRepositoryPath("")
+		if err != nil {
+			return
+		}
+		api.PersistNote(note, repoPath)
+		// Respond
+		c.JSON(http.StatusOK, gin.H{"status": "Note received"})
+	})
+
+	// Run the server on localhost:3010
+	r.Run("localhost:3010")
+}
